407a71f2989f611ddd62677b643e4e7aa44458f5
Student <example@example.com>
2019-12-13 16:01:33 -0800

    test passes as intended

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,7 +5,7 @@ require('Anagram')
 describe(Anagram) do
   describe('#initialize') do
     it('check if word is an Anagram') do
-      test = Anagram.new("beards", "bread")
+      test = Anagram.new("beard", "bread")
       puts "test"
       expect(test.find_ana?).to(eq(true))
     end
