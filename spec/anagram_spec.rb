3962fe579932df535a49e09965a97e6f58c8b591
Student <example@example.com>
2019-12-13 16:05:31 -0800

    test two fails as expected

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -10,4 +10,12 @@ describe(Anagram) do
       expect(test.find_ana?).to(eq(true))
     end
   end
+
+  describe('#initialize') do
+    it('check if word is an Anagram') do
+      test = Anagram.new("BEard", "bread")
+      puts "test"
+      expect(test.find_ana?).to(eq(true))
+    end
+  end
 end
