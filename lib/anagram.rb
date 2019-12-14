37bd45fdbca8d95ec4a114083c55ed36bfa56765
Student <example@example.com>
2019-12-13 16:00:59 -0800

    test fails as intended

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -46,21 +46,28 @@
 class Anagram
   attr_reader(:inputA, :inputB)
   attr_writer(:ana_counter)
-def initialize(inputA, inputB)
-  @inputA = inputA
-  @inputB = inputB
+  def initialize(inputA, inputB)
+    @inputA = inputA
+    @inputB = inputB
 
-end
+  end
   def find_ana?()
+    ana_counter = 0
     string = inputA.split("")
     string.each do |chr|
       if inputB.include?(chr)
         puts "yes"
+        ana_counter += 1
+        puts ana_counter
       else
         puts "no"
 
       end
-
+    end
+    if ana_counter === @inputA.length()
+      return true
+    else
+      return false
     end
 
   end
