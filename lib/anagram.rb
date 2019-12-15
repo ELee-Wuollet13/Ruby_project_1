f1e610b439bbabeeec57facb72478697c4f18bdc
Student <example@example.com>
2019-12-14 20:11:04 -0800

    first three objective compete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,39 +5,53 @@ class Anagram
     @inputA = inputA
     @inputB = inputB
 
+
+
+
   end
+
+
+
   def find_ana?()
+    # vowel_array = "a"
     if @inputA.length != @inputB.length()
       return "not an Anagram"
+    # elsif @inputA.exclude?(vowel_array) | @inputB.exclude?(vowel_array)
+    #   return "enter a real word"
+
     end
-    ana_counter = 0
 
-    stringB = @inputB.downcase.split("")
-    stringB.each do |chr|
-      if @inputA.downcase.include?(chr)
-        stringA = @inputA.downcase.split("")
-        stringA.each do |chr|
-          if @inputB.downcase.include?(chr)
-            puts "yes"
-            ana_counter += 1
-            puts ana_counter
-          else
-            puts "no"
 
-          end
+
+
+  ana_counter = 0
+
+  stringB = @inputB.downcase.split("")
+  stringB.each do |chr|
+    if @inputA.downcase.include?(chr)
+      stringA = @inputA.downcase.split("")
+      stringA.each do |chr|
+        if @inputB.downcase.include?(chr)
+          puts "yes"
+          ana_counter += 1
+          puts ana_counter
+        else
+          puts "no"
+
         end
       end
     end
-    if ana_counter === 0
-      @result = "anti_true"
-    elsif ana_counter / inputA.length() != @inputB.length()
-      @result = false
-      puts  ana_counter
-    elsif ana_counter / inputA.length() === @inputB.length()
-      @result = true
-    end
-    puts @result
-    return @result
   end
+  if ana_counter === 0
+    @result = "anti_true"
+  elsif ana_counter / inputA.length() != @inputB.length()
+    @result = false
+    puts  ana_counter
+  elsif ana_counter / inputA.length() === @inputB.length()
+    @result = true
+  end
+  puts @result
+  return @result
+end
 
 end
