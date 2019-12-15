6f7b3bfdcd149edac26d55bda0c2e796ff260e93
Student <example@example.com>
2019-12-14 19:00:37 -0800

    tests one and two failing as expected

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,29 +10,34 @@ class Anagram
     if @inputA.length != @inputB.length()
       return "not an Anagram"
     end
-
-
     ana_counter = 0
-    string = @inputA.downcase.split("")
-    string.each do |chr|
-      if @inputB.downcase.include?(chr)
-        puts "yes"
-        ana_counter += 1
-        puts ana_counter
-      else
-        puts "no"
 
+    stringB = @inputB.downcase.split("")
+    stringB.each do |chr|
+      if @inputA.downcase.include?(chr)
+        stringA = @inputA.downcase.split("")
+        stringA.each do |chr|
+          if @inputB.downcase.include?(chr)
+            puts "yes"
+            ana_counter += 1
+            puts ana_counter
+          else
+            puts "no"
+
+          end
+        end
       end
     end
-    if ana_counter != @inputB.length()
-      @result = false
-    elsif ana_counter === 0
-      puts  ana_counter
+    if ana_counter === 0
       @result = "anti_true"
+    elsif ana_counter != @inputB.length()
+      @result = false
+      puts  ana_counter
     elsif ana_counter === @inputB.length()
       @result = true
     end
     puts @result
-   return @result
+    return @result
   end
+
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,22 +4,22 @@ require('Anagram')
 
 describe(Anagram) do
   describe('#initialize') do
-    it('check if word is an Anagram') do
-      test = Anagram.new("beerd","bread")
+    it('test1 check if word is an Anagram') do
+      test = Anagram.new("bread", "beerd")
       puts "test1"
       expect(test.find_ana?).to(eq(true))
     end
   end
 
   describe('#initialize') do
-    it('check if word is an Anagram despite case') do
-      test = Anagram.new("bA","as")
+    it('test2 check if word is an Anagram despite case') do
+      test = Anagram.new("bAd","sad")
       puts "test2"
       expect(test.find_ana?).to(eq(true))
     end
   end
   describe('#initialize') do
-    it('check if word is an Antigram') do
+    it('test3 check if word is an Antigram') do
       test = Anagram.new("fall","sets")
       puts "test3"
       expect(test.find_ana?).to(eq("anti_true"))
