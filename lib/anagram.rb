ad99f8963a0be1596bdc59e109242ae67bc7966b
Student <example@example.com>
2020-01-02 15:42:51 -0800

    passes four tests for anagrams, case sensative and antigrams. counts words without voewls as words and does not yet remove non-letters

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,54 +4,41 @@ class Anagram
   def initialize(inputA, inputB)
     @inputA = inputA
     @inputB = inputB
-
-
-
-
   end
-
-
 
   def find_ana?()
     vowel_array = ["a", "e", "i", "o", "o", "u", "y"]
     if @inputA.length != @inputB.length()
       return "not an Anagram"
-    elsif @inputA != @inputB
-      return "enter a real word"
-
     end
 
+    ana_counter = 0
 
+    stringB = @inputB.downcase.split("")
+    stringB.each do |chr|
+      if @inputA.downcase.include?(chr)
+        stringA = @inputA.downcase.split("")
+        stringA.each do |chr|
+          if @inputB.downcase.include?(chr)
+            puts "yes"
+            ana_counter += 1
+            puts ana_counter
+          else
+            puts "no"
 
-
-  ana_counter = 0
-
-  stringB = @inputB.downcase.split("")
-  stringB.each do |chr|
-    if @inputA.downcase.include?(chr)
-      stringA = @inputA.downcase.split("")
-      stringA.each do |chr|
-        if @inputB.downcase.include?(chr)
-          puts "yes"
-          ana_counter += 1
-          puts ana_counter
-        else
-          puts "no"
-
+          end
         end
       end
     end
+    if ana_counter === 0
+      @result = "anti_true"
+    elsif ana_counter / inputA.length() != @inputB.length()
+      @result = false
+      puts  ana_counter
+    elsif ana_counter / inputA.length() === @inputB.length()
+      @result = true
+    end
+    puts @result
+    return @result
   end
-  if ana_counter === 0
-    @result = "anti_true"
-  elsif ana_counter / inputA.length() != @inputB.length()
-    @result = false
-    puts  ana_counter
-  elsif ana_counter / inputA.length() === @inputB.length()
-    @result = true
-  end
-  puts @result
-  return @result
-end
-
 end
