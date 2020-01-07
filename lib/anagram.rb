a29a3ba1acf89a694c2e415a584f0ffd04b35a06
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2020-01-07 00:34:34 -0800

    tests corrected and main refactored

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,36 +6,38 @@ class Anagram
     @inputB = inputB.downcase.gsub(/([^a-z])/, "")
   end
 
-  def find_ana?()
+  def is_word()
     if @inputA[/(['aeiou'])/] && @inputB[/(['aeiou'])/]
-      puts "real word"
     else
       return "not a real word"
     end
-      if @inputA.length != @inputB.length()
-        return "not an Anagram"
-      end
+  end
 
-      ana_counter = 0
+  def is_length
+    if @inputA.length != @inputB.length()
+      return "not an Anagram"
+    end
+  end
 
-      stringB = @inputB.split("")
-      stringB.each do |chr|
-        if @inputA.downcase.include?(chr)
-          stringA = @inputA.split("")
-          stringA.each do |chr|
-            if @inputB.downcase.include?(chr)
-              ana_counter += 1
-            end
+  def find_ana()
+    ana_counter = 0
+
+    @stringB.split("").each do |chr|
+      if @inputA.downcase.include?(chr)
+        @stringA.split("").each do |chr|
+          if @inputB.downcase.include?(chr)
+            ana_counter += 1
           end
         end
       end
-      if ana_counter === 0
-        @result = "anti_true"
-      elsif ana_counter / inputA.length() != @inputB.length()
-        @result = false
-      elsif ana_counter / inputA.length() === @inputB.length()
-        @result = true
-      end
-      return @result
+    end
+    if ana_counter === 0
+      @result = "anti_true"
+    elsif ana_counter / inputA.length() != @inputB.length()
+      @result = false
+    elsif ana_counter / inputA.length() === @inputB.length()
+      @result = true
+    end
+    return @result
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,45 +4,39 @@ require('Anagram')
 
 describe(Anagram) do
   describe('#initialize') do
-    it('test1 check if word is an Anagram') do
+    it('check if word is an Anagram') do
       test = Anagram.new("bread", "beard")
-      puts "test1"
       expect(test.find_ana?).to(eq(true))
     end
   end
 
   describe('#initialize') do
-    it('test2 check if word is an Anagram despite case') do
+    it('check if word is an Anagram despite case') do
       test = Anagram.new("bAd","bad")
-      puts "test2"
       expect(test.find_ana?).to(eq(true))
     end
   end
   describe('#initialize') do
-    it('test2 check if word is an Anagram despite case') do
+    it('check if word is an Anagram despite case') do
       test = Anagram.new("brgd","brgd")
-      puts "test3"
-      expect(test.find_ana?).to(eq(true))
+      expect(test.find_ana?).to(eq("not a real word"))
     end
   end
   describe('#initialize') do
-    it('test3 check if word is an Antigram') do
+    it('check if word is an Antigram') do
       test = Anagram.new("fall","sets")
-      puts "test4"
       expect(test.find_ana?).to(eq("anti_true"))
     end
   end
   describe('#initialize') do
     it('check if word is an Anagram despite nonletters') do
       test = Anagram.new("Beard","B4rea@d")
-      puts "test5"
       expect(test.find_ana?).to(eq(true))
     end
   end
   describe('#initialize') do
     it('check if word is an Anagram despite nonletters') do
       test = Anagram.new("Church of Scientology", "rich chosen goofy cult")
-      puts "test6"
       expect(test.find_ana?).to(eq(true))
     end
   end
