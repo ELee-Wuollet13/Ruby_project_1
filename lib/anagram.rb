ad847160f91e01d1b4364fd2374a1c0729621018
Student <example@example.com>
2020-01-07 17:28:56 -0800

    pass tests, change find_ana to non boolean, edit down for ease

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,30 +11,30 @@ class Anagram
     else
       return "not a real word"
     end
-      if @inputA.length != @inputB.length()
-        return "not an Anagram"
-      end
+    if @inputA.length != @inputB.length()
+      return "not an Anagram"
+    end
 
-      ana_counter = 0
+    ana_counter = 0
 
-      stringB = @inputB.split("")
-      stringB.each do |chr|
-        if @inputA.downcase.include?(chr)
-          stringA = @inputA.split("")
-          stringA.each do |chr|
-            if @inputB.downcase.include?(chr)
-              ana_counter += 1
-            end
+    stringB = @inputB.split("")
+    stringB.each do |chr|
+      if @inputA.downcase.include?(chr)
+        stringA = @inputA.split("")
+        stringA.each do |chr|
+          if @inputB.downcase.include?(chr)
+            ana_counter += 1
           end
         end
       end
-      if ana_counter === 0
-        @result = "anti_true"
-      elsif ana_counter / inputA.length() != @inputB.length()
-        @result = false
-      elsif ana_counter / inputA.length() === @inputB.length()
-        @result = true
-      end
-      return @result
+    end
+    if ana_counter === 0
+      @result = "anti_true"
+    elsif ana_counter / inputA.length() != @inputB.length()
+      @result = false
+    elsif ana_counter / inputA.length() === @inputB.length()
+      @result = true
+    end
+    return @result
   end
 end
